961e257e6b75bad4b72306298280dc8a568d414a
DeepaSh <deepa.shammi@gmail.com>
2019-09-18 08:11:05 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,36 @@
+class A {
+ public static void main(String args[]){
+     System.out.println("Hello World");
+ }
+}
+
+class B {
+  public static void main(String args[]){
+      System.out.println("Hi All");
+  }
+}
+
+Class C {
+  public static void main(String args[]){
+    Sytem.out.println("Bye All")
+  }
+}
+
+Class D {
+  public static void main(String args[]){
+    System.out.println("I am in class D")
+  }
+}
+
+Class E {
+  public static void main(String args[]){
+    System.out.println("I am in Class E")
+  }
+}
+
+
+Class F {
+  public static void main(String args[]){
+    System.out.println("I am in Class F")
+  }
+}
